d4a6a4d12577340d3c4ac7e43de89d00d5e91d61
Zach Fogg <zach.fogg@gmail.com>
2013-12-11 21:34:27 -0500

    Merged.

--- server.py
+++ server.py
@@ -1,17 +1,38 @@
-from bottle import route, run, static_file
-import os
+from bottle import route, static_file, template, request, run
+
+from os import listdir
+from os.path import realpath, dirname, splitext, join, isfile
 
 
-path = os.path.dirname(os.path.realpath(__file__))
+path           = dirname(realpath(__file__))
+datastore_path = path + '/datastore'
 
 
 @route('/')
 def route_root():
-    return static_file("index.html", root=path + '/public')
+    return template('public/index.html', files=list_files())
 
 @route('/download/<filepath:path>')
 def download_file(filepath):
-    return static_file(filepath, root=path + '/datastore')
+    return static_file(filepath, root=datastore_path)
+
+@route('/upload', method='POST')
+def upload_file():
+    upload = request.files.get('upload')
+    name, ext = splitext(upload.filename)
+    upload.save(datastore_path)
+    return 'Success!'
+
+@route('/debug-list_files')
+def list_files():
+    return [f for f
+            in listdir(datastore_path)
+            if isfile(join(datastore_path, f))]
+
+@route('/debug-print_path')
+def print_path():
+    # FIXME: debug
+    return datastore_path
 
 
  # Static Routes
@@ -33,4 +54,4 @@ def fonts(filename):
     return static_file(filename, root=path + '/public')
 
 
-run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8080, debug=True, reloader=True)
