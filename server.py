784e83b1c2f7c51f24b6440b16305572c268092c
Craig Palermo <cpalermo2@gmail.com>
2013-12-11 21:30:08 -0500

    fixed bug in expire files and made it run every 60 seconds in its own thread

--- server.py
+++ server.py
@@ -5,11 +5,12 @@ from os import walk
 from datetime import datetime, timedelta
 
 import os, os.path, time
-
+import threading
+import traceback
 
 path = os.path.dirname(os.path.realpath(__file__))
 datastore_path = path + '/datastore'
-retention_time = 10 # in minutes
+retention_time = 1 # in minutes
 
 @route('/')
 def route_root():
@@ -57,15 +58,17 @@ def expire_files():
     Removes from the datastore directory all files that have modified
     time older than retention_time
     '''
-    files = list_files()
-    for f in files:
-        try:
-            temp = time.ctime(os.path.getmtime(datastore_path + '/' + f))
-            age = datetime.now() - datetime.strptime(temp, "%a %b %d %H:%M:%S %Y")
-            if age > timedelta(minutes=retention_time):
-                os.remove(datastore_path + '/' + f)
-        except:
-            return "error statting files"
+    while(True):
+        files = list_files()
+        for f in files:
+            try:
+                temp = time.ctime(os.path.getmtime(datastore_path + '/' + f))
+                age = datetime.now() - datetime.strptime(temp, "%a %b %d %H:%M:%S %Y")
+                if age > timedelta(minutes=retention_time):
+                    os.remove(datastore_path + '/' + f)
+            except:
+                print "error statting files - " + traceback.format_exc()
+        time.sleep(5)
 
 # Static Routes
 @route('/<filename:re:.*\.js>')
@@ -85,5 +88,10 @@ def images(filename):
 def fonts(filename):
     return static_file(filename, root=path + '/public')
 
-
+# Initialize the server
+print "Starting server"
+datetime.strptime('2013-01-01', '%Y-%m-%d')
+expire_files_thread = threading.Thread(target=expire_files)
+expire_files_thread.start()
 run(host='localhost', port=8080, debug=True)
+
