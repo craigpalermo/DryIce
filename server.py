10ce4cb3e34506cbc02f6e71e2620436d846bc2a
Zach Fogg <zach.fogg@gmail.com>
2013-12-11 20:11:52 -0500

    Support for static files.

--- server.py
+++ server.py
@@ -1,15 +1,36 @@
 from bottle import route, run, static_file
 import os
 
+
 path = os.path.dirname(os.path.realpath(__file__))
+
+
+@route('/')
+def route_root():
+    return static_file("index.html", root=path + '/public')
 
 @route('/download/<filepath:path>')
 def download_file(filepath):
     return static_file(filepath, root=path + '/datastore')
 
-@route('/path')
-def print_path():
-    return path + '/datastore'
+
+ # Static Routes
+@route('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root=path + '/public')
+
+@route('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    print(filename)
+    return static_file(filename, root=path + '/public')
+
+@route('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root=path + '/public')
+
+@route('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root=path + '/public')
+
 
 run(host='localhost', port=8080, debug=True)
-
