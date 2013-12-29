891d4743883ddf52570bf57920c35e54e277101f
Craig Palermo <cpalermo2@gmail.com>
2013-12-28 21:53:18 -0500

    implemented ez_links, added instructions to main page, changed style of link table

--- src/server.py
+++ src/server.py
@@ -8,6 +8,8 @@ from werkzeug.exceptions import RequestEntityTooLarge
 from time          import sleep
 from threading     import Thread
 from time          import mktime
+from random_words  import RandomWords
+from redis         import Redis
 
 from fileutils     import *
 from converter     import RegexConverter
@@ -25,33 +27,7 @@ app.config['UPLOAD_FOLDER'] = PATH_DATASTORE
 app.config['MAX_CONTENT_LENGTH'] = MB_UPLOAD_LIMIT * 1024 * 1024 
 app.secret_key = '8s9fs9fs09dfi9324s'
 
-# S3 Storage Setup
-def generate_upload_form(session_id):
-    policy = """
-    {"expiration": "%(expires)s",
-        "conditions": [ 
-            {"bucket": "%(bucket)s"}, 
-            ["starts-with", "$key", ""],
-            {"acl": "%(acl)s"},
-            {"success_action_redirect": "%(success_redirect)s"},
-            ["starts-with", "$Content-Type", ""],
-            ["content-length-range", 0, %(length_range)s],
-            {"x-amz-meta-session_id": "%(session_id)s"}
-        ]
-    }
-    """
-    policy = policy%{
-        "expires":"2015-01-01T00:00:00Z",
-        "bucket": BUCKET,
-        "acl": "public-read",
-        "success_redirect": "/",
-        "length_range": app.config['MAX_CONTENT_LENGTH'],
-        "session_id": session_id
-    }
-
-    policy = base64.b64encode(policy)
-    signature = base64.b64encode(hmac.new(SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
-    return {'policy':policy, 'signature':signature}
+r_server = Redis("localhost")
 
 # Routes ------------------------------------------------------------------
 @app.route('/')
@@ -70,9 +46,18 @@ def route_root():
 
     # pick out which files were uploaded from the current session
     for f in temp:
+        f_name = f.get('name')
         f['expire_time'] = f['created'] + \
                             timedelta(minutes=FILE_RETENTION_TIME)
         f['nice_name'] = f.get('name')[37:] # strip folder prefix
+
+        if not r_server.exists(f_name):
+            ez_link = generate_ez_link()
+            r_server.set(f_name, ez_link)
+            r_server.set(ez_link, f_name)
+
+        f['ez_link'] = r_server.get(f_name)
+        
         files.append(f)
     
     template_data = {
@@ -105,17 +90,53 @@ def clear_session():
     session.clear()
     return route_root()
 
-@app.route('/file/<path:filename>/')
-def route_file(filename):
+@app.route('/<path:ez_link>/')
+def route_file(ez_link):
     bucket = setup_bucket()
+    filename = r_server.get(ez_link)
     key = bucket.new_key(filename)
     url = key.generate_url(expires_in=(FILE_RETENTION_TIME * 60),query_auth=False, force_http=True)
     filename = filename[37:] # strip folder prefix
     template_data = {'filename': filename, 'url': url}
     return render('file.html', template_data)
 
+@app.route('/random/')
+def route_random():
+    return generate_ez_link()
 
 # Other Functions --------------------------------------------------------
+# S3 Storage Setup
+def generate_upload_form(session_id):
+    policy = """
+    {"expiration": "%(expires)s",
+        "conditions": [ 
+            {"bucket": "%(bucket)s"}, 
+            ["starts-with", "$key", ""],
+            {"acl": "%(acl)s"},
+            {"success_action_redirect": "%(success_redirect)s"},
+            ["starts-with", "$Content-Type", ""],
+            ["content-length-range", 0, %(length_range)s],
+            {"x-amz-meta-session_id": "%(session_id)s"}
+        ]
+    }
+    """
+    policy = policy%{
+        "expires":"2015-01-01T00:00:00Z",
+        "bucket": BUCKET,
+        "acl": "public-read",
+        "success_redirect": "/",
+        "length_range": app.config['MAX_CONTENT_LENGTH'],
+        "session_id": session_id
+    }
+
+    policy = base64.b64encode(policy)
+    signature = base64.b64encode(hmac.new(SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+    return {'policy':policy, 'signature':signature}
+
+def generate_ez_link():
+    rw = RandomWords()
+    return rw.random_word().capitalize() + rw.random_word().capitalize()
+
 def update_reset_time():
     '''
     Updates the session's reset time and quota when the current time
