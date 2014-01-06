1247845744c1be02cdb60a6c55e8c4114f0017b4
Craig Palermo <cpalermo2@gmail.com>
2014-01-05 22:57:10 -0500

    added a function back temporarily so the site still functions on the dev branch

--- DryIce/utils/session_utils.py
+++ DryIce/utils/session_utils.py
@@ -22,6 +22,33 @@ def get_session_id(request):
 # S3 Storage Setup
 def generate_upload_form(session_id):
     policy = """
+            {"expiration": "%(expires)s",
+             "conditions": [ 
+                {"bucket": "%(bucket)s"}, 
+                ["starts-with", "$key", ""],
+                {"acl": "%(acl)s"},
+                {"success_action_redirect": "%(success_redirect)s"},
+                ["starts-with", "$Content-Type", ""],
+                ["content-length-range", 0, %(length_range)s],
+                {"x-amz-meta-session_id": "%(session_id)s"}
+             ]
+            }
+                                                                                        """
+    policy = policy%{
+        "expires":"2015-01-01T00:00:00Z",
+        "bucket": BUCKET,
+        "acl": "public-read",
+        "success_redirect": "/",
+        "length_range": MAX_CONTENT_LENGTH,
+        "session_id": session_id
+    }
+    
+    policy = base64.b64encode(policy)
+    signature = base64.b64encode(hmac.new(SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+    return {'policy':policy, 'signature':signature}
+
+def generate_upload_form_data(session_id):
+    policy = """
     {"expiration": "%(expires)s",
         "conditions": [ 
             {"bucket": "%(bucket)s"}, 
